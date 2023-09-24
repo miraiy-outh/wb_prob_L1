62c877759bf706017cd5743a1aadfbdc6eaf81e5
miraiy-outh <shokolatemeur@mail.ru>
2023-09-24 23:19:09 +0300

    added 3&4

---
+++ 3math.js
@@ -0,0 +1,127 @@
+
+const MathX = (function () {
+    const fibRow = []; // Массив, хранящий в себе вычисленные числа Фибоначчи.
+    const primeRow = [2, 3]; // Массив, хранящий в себе вычисленные простые числа.
+
+    /**
+     * Подсчет n-го числа Фибоначчи.
+     *
+    * @param {number} n - Переданный номер.
+    * @return {number} - n-е число Фибоначчи.
+    */
+    function calcFib(n) {
+        if (n <= 1) {
+            return n;
+        }
+        // Если в массиве чисел Фибоначчи уже есть вычисленное значение, то возвращаем его, иначе - вычисляем.
+        if (fibRow[n]) {
+            return fibRow[n];
+        }
+        fibRow[n] = calcFib(n - 1) + calcFib(n - 2);
+        return fibRow[n];
+    }
+
+    /**
+     * Подсчет всех чисел Фибоначчи до n-го.
+     *
+    * @param {number} n - Переданный номер.
+    * @return {number[]} - Массив чисел Фибоначчи до n-го.
+    */
+    function getFibonacciAll(n) {
+        if (n <= 0) {
+            return undefined;
+        }
+        var result = [];
+        for (let i = 0; i <= n; i++) {
+            result.push(calcFib(i));
+        }
+        return result;
+    }
+
+    /**
+     * Получение n-го числа Фибоначчи.
+     *
+    * @param {number} n - Переданный номер.
+    * @return {number[]} - n-е число Фибоначчи.
+    */
+    function getFibonacci(n) {
+        if (n <= 0) {
+            return undefined;
+        }
+        return calcFib(n);
+    }
+
+    /**
+    * Проверка на простое число.
+    *
+    * @param {number} num - Переданное число.
+    * @return {boolean} - Результат проверки.
+    */
+    function isPrime(num) {
+        // Исключаем из проверки 1 и 2.
+        if (num === 1) {
+            return false;
+        }
+        if (num === 2) {
+            return true;
+        }
+        /* Если число N равно произведению двух других, то одно из них не больше корня из N, а другое не меньше корня из N.
+           Поэтому после этого значения нет смысла искать делители. */
+        var sqrtNum = Math.floor(Math.sqrt(num));
+
+        for (var i = 2; i < sqrtNum + 1; i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+    * Получение всех простых чисел до n-го.
+    *
+    * @param {number} n - Переданный номер.
+    * @return {number[]} - Массив простых чисел до n-го.
+    */
+    function getPrimeAll(n) {
+        if (n < 1) {
+            return null;
+        }
+
+        // Находим следующее нечетное число за последним в массиве простых чисел.
+        var maybePrime = primeRow[primeRow.length - 1] + 2;
+
+        // Добавляем простые числа в массив, пока не дойдем до n-го простого числа.
+        while (primeRow.length < n) {
+            if (isPrime(maybePrime)) {
+                primeRow.push(maybePrime);
+            }
+            maybePrime += 2;
+        }
+
+        // если длина массива простых чисел больше n, выводим только часть массива до n.
+        if (primeRow.length > n) {
+            return primeTmp = primeRow.slice(0, n);
+        }
+
+        return primeRow;
+    }
+
+    // Функция для получения N-го простого числа.
+    function getPrime(n) {
+        if (n <= 1) {
+            return null;
+        }
+        const primes = getPrimeAll(n);
+        return primes[n - 1];
+    }
+
+    return { getFibonacciAll, getFibonacci, getPrimeAll, getPrime };
+})();
+
+console.log('20 чисел Фибоначчи:\n', MathX.getFibonacciAll(20));
+console.log('9 чисел Фибоначчи:\n', MathX.getFibonacciAll(9));
+console.log('10-е число Фибоначчи:\n', MathX.getFibonacci(10));
+console.log('20 простых чисел:\n', MathX.getPrimeAll(20));
+console.log('5 простых чисел:\n', MathX.getPrimeAll(5));
+console.log('10-е простое число:\n', MathX.getPrime(10));
---
+++ 4ending.js
@@ -0,0 +1,57 @@
+massive1 = ['сообщение', 'сообщения', 'сообщений'];
+massive2 = ['пользователь', 'пользователя', 'пользователей'];
+
+const ChangeEndingModule = (function () {
+    /**
+    * Получение правильной формы числа в зависимости от количества.
+    * Зависимость формы слова от количества:
+    * если 2 последние цифры: 11-19 - мн. ч. род. падеж;
+    * иначе, если последняя цифра:
+    * 1 - ед. ч. им. падеж;
+    * 2-4 - ед. ч. род. падеж;
+    * 5-9, 0 - мн. ч. род. падеж;
+    * 
+    * @param {number} num - Переданное количество.
+    * @param {string[]} - Массив из слов в 3 формах (ед. ч. им. падеж, ед. ч. род. падеж, мн. ч. род. падеж).
+    * @return {string} - Слово в правильной форме.
+    */
+    function changeEnding(num, wordsMassive) {
+        if (wordsMassive.length < 3) {
+            return undefined;
+        }
+        if (num < 0) {
+            num = num - 2 * num; // если число отрицательное, делаем его положительным
+        }
+
+        num = num % 100;
+        if ((num >= 11 && num <= 19)) {
+            return wordsMassive[2];
+        }
+        else {
+            num = num % 10;
+            if (num === 1) {
+                return wordsMassive[0];
+            }
+            if (num >= 2 && num <= 4) {
+                return wordsMassive[1];
+            }
+            if (num >= 5 || num === 0) {
+                return wordsMassive[2];
+            }
+        }
+
+        return undefined;
+    }
+
+    return { changeEnding };
+})();
+
+console.log(112, ChangeEndingModule.changeEnding(112, massive1));
+console.log(-112, ChangeEndingModule.changeEnding(-112, massive1));
+console.log(122, ChangeEndingModule.changeEnding(122, massive1));
+console.log(12, ChangeEndingModule.changeEnding(12, massive1));
+console.log(1012, ChangeEndingModule.changeEnding(1012, massive2));
+console.log(1024, ChangeEndingModule.changeEnding(1024, massive2));
+console.log(1026, ChangeEndingModule.changeEnding(1026, massive2));
+console.log(-1026, ChangeEndingModule.changeEnding(-1026, massive2));
+console.log(121, ChangeEndingModule.changeEnding(121, massive2));
